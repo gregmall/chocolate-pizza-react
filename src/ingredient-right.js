bbf5ab875e41299b2c25197cef795b6c99aa5970
Greg Mall <gregmall157@gmail.com>
2020-08-03 16:35:16 -0700

    added right data

---
+++ src/ingredient-right.js
@@ -0,0 +1,11 @@
+const right = [ 
+    '1 1/2 cups heavy cream',
+    '1/2 granulated sugar',
+    '2 egg yolks',
+    '1 lemon, juiced',
+    '2 tbsp butter',
+    '1 cup honey roasted pecans, roughly chopped'
+
+]
+
+export default right;
